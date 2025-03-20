bc5477655108999f5a416cd4c1705477a35ae6b2
Hknots <hknots99@gmail.com>
2025-03-20 10:47:38 +0100

    refactor: addTopic & removeTopic now requires TopicNameParameters instead of string

--- src/main/kotlin/no/fintlabs/core/kafka/utils/CoreTopicService.kt
+++ src/main/kotlin/no/fintlabs/core/kafka/utils/CoreTopicService.kt
@@ -20,36 +20,13 @@ class CoreTopicService(
     private val topicRetentionMap = mutableMapOf<String, Long>()
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    /**
-     * Adds a topic to the retention map.
-     *
-     * **Note:** This method does not ensure that the topic is created in Kafka.
-     *
-     * @param topicName The name of the topic.
-     * @param retentionTimeInMs The retention time in milliseconds for the topic.
-     * @return The previous retention time associated with the topic, or null if it was not present.
-     */
-    fun addTopic(topicName: String, retentionTimeInMs: Long): Long? =
-        topicRetentionMap.put(topicName, retentionTimeInMs)
 
-    /**
-     * Removes a topic from the retention map.
-     *
-     * **Note:** This method does not remove the topic from Kafka.
-     *
-     * @param topicName The name of the topic.
-     * @return The retention time associated with the topic if it existed, or null otherwise.
-     */
-    fun removeTopic(topicName: String): Long? =
-        topicRetentionMap.remove(topicName)
+    fun addTopic(topicNameParameters: TopicNameParameters, retentionTimeInMs: Long): Long? =
+        topicRetentionMap.put(topicNameParameters.topicName, retentionTimeInMs)
 
-    /**
-     * Ensures that a Kafka topic is created with the specified retention time.
-     * If the topic exists with a different retention time, it is updated.
-     *
-     * @param topicNameParameters The topic parameters which include the topic name.
-     * @param retentionTimeInMs The desired retention time in milliseconds.
-     */
+    fun removeTopic(topicNameParameters: TopicNameParameters): Long? =
+        topicRetentionMap.remove(topicNameParameters.topicName)
+
     fun ensureTopic(topicNameParameters: TopicNameParameters, retentionTimeInMs: Long) {
         when (topicNameParameters) {
             is EntityTopicNameParameters -> ensureTopicInternal(topicNameParameters, retentionTimeInMs) { params, retention ->
@@ -68,16 +45,6 @@ class CoreTopicService(
         }
     }
 
-    /**
-     * Helper method to ensure a topic using a specific service action.
-     *
-     * This method checks if the topic already exists in the retention map.
-     * If it does not exist or if the retention time has changed, it will execute the provided action.
-     *
-     * @param topicParameters The topic parameters containing the topic name.
-     * @param retentionTimeInMs The desired retention time.
-     * @param ensureAction A lambda function that calls the appropriate topic service.
-     */
     private fun <T : TopicNameParameters> ensureTopicInternal(
         topicParameters: T,
         retentionTimeInMs: Long,
@@ -90,22 +57,9 @@ class CoreTopicService(
         } else logger.debug("Topic $topicName is already ensured with retention time $retentionTimeInMs")
     }
 
-    /**
-     * Checks if the retention time for the given topic is different from the provided retention time.
-     *
-     * @param topicName The name of the topic.
-     * @param newRetentionTimeInMs The new retention time to compare.
-     * @return True if the topic does not exist or if the existing retention time differs from newRetentionTime, false otherwise.
-     */
     fun hasDifferentRetentionTime(topicName: String, newRetentionTimeInMs: Long): Boolean =
         topicRetentionMap[topicName] != newRetentionTimeInMs
 
-    /**
-     * Retrieves the retention time for a given topic.
-     *
-     * @param topicName The name of the topic.
-     * @return The retention time if present, null otherwise.
-     */
     fun getRetentionTime(topicName: String): Long? = topicRetentionMap[topicName]
 
 }
