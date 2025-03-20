e10ca6063874a5a796432bdd49cbcc80cec24d0e
Hknots <hknots99@gmail.com>
2025-03-20 09:59:22 +0100

    initial commit

---
+++ src/test/kotlin/no/fintlabs/core/kafka/utils/FintCoreKafkaUtilsApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.fintlabs.core.kafka.utils
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FintCoreKafkaUtilsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
